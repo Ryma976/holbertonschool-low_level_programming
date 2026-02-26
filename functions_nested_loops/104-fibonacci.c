0b8924941871ae6b4c888dcd48121fd9c5de03b2
reem <ryma6110@gmail.com>
2026-02-26 21:03:13 +0000

    Task 16: First 98 Fibonacci numbers with split addition

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,38 +7,29 @@
  */
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, next, i;
-	unsigned long f1_h, f1_l, f2_h, f2_l, next_h, next_l;
-	unsigned long head = 1000000000; /* مقسم لـ 9 خانات */
+	unsigned long f1 = 1, f2 = 2, i, head = 1000000000;
+	unsigned long f1_h, f1_l, f2_h, f2_l, n_h, n_l;
 
-	/* طباعة أول 91 رقماً (التي لا تتجاوز سعة unsigned long) */
 	printf("%lu, %lu", f1, f2);
 	for (i = 3; i <= 91; i++)
 	{
-		next = f1 + f2;
-		printf(", %lu", next);
-		f1 = f2;
-		f2 = next;
+		f2 = f1 + f2;
+		f1 = f2 - f1;
+		printf(", %lu", f2);
 	}
-
-	/* تقسيم الأرقام الحالية للبدء في الحساب المتقدم */
 	f1_h = f1 / head;
 	f1_l = f1 % head;
 	f2_h = f2 / head;
 	f2_l = f2 % head;
-
 	for (i = 92; i <= 98; i++)
 	{
-		next_l = f1_l + f2_l;
-		next_h = f1_h + f2_h + (next_l / head);
-		next_l %= head;
-
-		printf(", %lu%09lu", next_h, next_l);
-
+		n_h = f1_h + f2_h + (f1_l + f2_l) / head;
+		n_l = (f1_l + f2_l) % head;
+		printf(", %lu%09lu", n_h, n_l);
 		f1_h = f2_h;
 		f1_l = f2_l;
-		f2_h = next_h;
-		f2_l = next_l;
+		f2_h = n_h;
+		f2_l = n_l;
 	}
 	printf("\n");
 	return (0);
