30549291bc001a2dd0a8d7395d8264313c3709cc
reem <ryma6110@gmail.com>
2026-03-03 20:26:47 +0000

    Task 10: Implement _atoi function

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	unsigned int res = 0;
+	int started = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			res = (res * 10) + (s[i] - '0');
+		}
+		else if (started)
+		{
+			break;
+		}
+		i++;
+	}
+
+	return (res * sign);
+}
