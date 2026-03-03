de9f83e2458718c85211387c00be1b2925b9c37f
reem <ryma6110@gmail.com>
2026-03-03 19:58:57 +0000

    Fix logic in print_rev task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * print_rev - prints a string in reverse, followed by a new line.
+ * @s: the string to be printed.
+ *
+ * Return: void.
+ */
 void print_rev(char *s)
 {
 	int i = 0;
