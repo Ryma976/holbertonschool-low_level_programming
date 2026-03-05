2e46bd1c74d6de7214791c8cabbe49ecf3ac138d
reem <ryma6110@gmail.com>
2026-03-05 21:16:14 +0000

    Fixed Betty warnings: lines over 80 characters

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -15,32 +15,26 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		return;
 	}
-
 	for (offset = 0; offset < size; offset += 10)
 	{
-		/* 1. طباعة العنوان (Position) مكون من 8 خانات */
 		printf("%08x: ", offset);
-
-		/* 2. طباعة المحتوى الست عشري (Hex) كل بايتين معاً */
 		for (i = 0; i < 10; i++)
 		{
 			if ((i + offset) >= size)
 				printf("  ");
 			else
 				printf("%02x", b[i + offset]);
-
 			if (i % 2 != 0)
 				printf(" ");
 		}
-
-		/* 3. طباعة النص القابل للقراءة (Printable characters) */
 		for (j = 0; j < 10; j++)
 		{
-			if ((j + offset) >= size)
-				break;
+			int pos = j + offset;
 
-			if (b[j + offset] >= 32 && b[j + offset] <= 126)
-				printf("%c", b[j + offset]);
+			if (pos >= size)
+				break;
+			if (b[pos] >= 32 && b[pos] <= 126)
+				printf("%c", b[pos]);
 			else
 				printf(".");
 		}
