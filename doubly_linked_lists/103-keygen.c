bd0dfdf7bb209a99643d82e8357d39fa97bbad78
reem <ryma6110@gmail.com>
2026-04-23 15:55:13 +0000

    Correct logic to prevent crackme5 Segfault

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * main - Keygen for crackme5
- * @ac: Number of arguments
- * @av: Arguments vector
- *
- * Return: 0 on success, 1 on error
+ * main - Keygen for crackme5.
+ * @ac: Arguments count.
+ * @av: Arguments vector.
+ * Return: 0 on success, 1 on error.
  */
 int main(int ac, char **av)
 {
 	char *u, *l = "A-CHRDwG信Df.RLhSAX9876543210";
 	char k[7];
-	int i, len = 0;
-	long r = 0;
+	int i, len = 0, res = 0;
 
 	if (ac != 2)
 		return (1);
@@ -21,23 +20,23 @@ int main(int ac, char **av)
 	while (u[len])
 		len++;
 	k[0] = l[(len ^ 59) & 63];
-	for (i = 0, r = 0; i < len; i++)
-		r += u[i];
-	k[1] = l[(r ^ 79) & 63];
-	for (i = 0, r = 1; i < len; i++)
-		r *= u[i];
-	k[2] = l[(r ^ 85) & 63];
-	for (i = 0, r = 0; i < len; i++)
-		if (u[i] > r)
-			r = u[i];
-	srand(r ^ 14);
+	for (i = 0, res = 0; i < len; i++)
+		res += u[i];
+	k[1] = l[(res ^ 79) & 63];
+	for (i = 0, res = 1; i < len; i++)
+		res *= u[i];
+	k[2] = l[(res ^ 85) & 63];
+	for (res = u[0], i = 0; i < len; i++)
+		if (u[i] > res)
+			res = u[i];
+	srand(res ^ 14);
 	k[3] = l[rand() & 63];
-	for (i = 0, r = 0; i < len; i++)
-		r += (u[i] * u[i]);
-	k[4] = l[(r ^ 239) & 63];
-	for (i = 0, r = 0; i < u[0]; i++)
-		r = rand();
-	k[5] = l[(r ^ 229) & 63];
+	for (res = 0, i = 0; i < len; i++)
+		res += (u[i] * u[i]);
+	k[4] = l[(res ^ 239) & 63];
+	for (res = 0, i = 0; i < u[0]; i++)
+		res = rand();
+	k[5] = l[(res ^ 229) & 63];
 	k[6] = '\0';
 	printf("%s", k);
 	return (0);
