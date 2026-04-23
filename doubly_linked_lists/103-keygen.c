1f7cc0617f3d5d221af4d3dad0446468f07a3126
reem <ryma6110@gmail.com>
2026-04-23 16:00:47 +0000

    Complete fresh rebuild of task 11

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 
+/**
+ * main - Generates a key for crackme5.
+ * @ac: The number of arguments.
+ * @av: The arguments vector.
+ * Return: 0 on success, 1 on error.
+ */
 int main(int ac, char **av)
 {
-    char *u, *l = "A-CHRDwG信Df.RLhSAX9876543210";
-    char k[7];
-    int i, len = 0, res = 0;
+	char *u, *l = "A-CHRDwG信Df.RLhSAX9876543210";
+	char k[7];
+	int i, len = 0, res = 0;
 
-    if (ac != 2) return (1);
-    u = av[1];
-    len = strlen(u);
-    k[0] = l[(len ^ 59) & 63];
-    for (i = 0, res = 0; i < len; i++) res += u[i];
-    k[1] = l[(res ^ 79) & 63];
-    for (i = 0, res = 1; i < len; i++) res *= u[i];
-    k[2] = l[(res ^ 85) & 63];
-    for (res = u[0], i = 0; i < len; i++) if (u[i] > res) res = u[i];
-    srand(res ^ 14);
-    k[3] = l[rand() & 63];
-    for (res = 0, i = 0; i < len; i++) res += (u[i] * u[i]);
-    k[4] = l[(res ^ 239) & 63];
-    for (res = 0, i = 0; i < u[0]; i++) res = rand();
-    k[5] = l[(res ^ 229) & 63];
-    k[6] = '\0';
-    printf("%s", k);
-    return (0);
+	if (ac != 2)
+		return (1);
+	u = av[1];
+	while (u[len])
+		len++;
+	k[0] = l[(len ^ 59) & 63];
+	for (i = 0, res = 0; i < len; i++)
+		res += u[i];
+	k[1] = l[(res ^ 79) & 63];
+	for (i = 0, res = 1; i < len; i++)
+		res *= u[i];
+	k[2] = l[(res ^ 85) & 63];
+	for (res = u[0], i = 0; i < len; i++)
+		if (u[i] > res)
+			res = u[i];
+	srand(res ^ 14);
+	k[3] = l[rand() & 63];
+	for (res = 0, i = 0; i < len; i++)
+		res += (u[i] * u[i]);
+	k[4] = l[(res ^ 239) & 63];
+	for (res = 0, i = 0; i < u[0]; i++)
+		res = rand();
+	k[5] = l[(res ^ 229) & 63];
+	k[6] = '\0';
+	printf("%s", k);
+	return (0);
 }
--- doubly_linked_lists/README.md
+++ doubly_linked_lists/README.md
@@ -1,1 +1,1 @@
-Doubly linked lists project
+Doubly Linked Lists Project
