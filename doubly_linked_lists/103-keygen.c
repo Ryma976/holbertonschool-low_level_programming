d13799db1d0a7deb808ba9a20f075c9a7737ea48
reem <ryma6110@gmail.com>
2026-04-23 15:20:45 +0000

    Submit keygen and advanced tasks

---
+++ doubly_linked_lists/103-keygen.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Generates a key for crackme5 based on a username.
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	char *u;
+	char *l = "A-CHRDwG信Df.RLhSAX9876543210";
+	char k[7];
+	int len, i, r;
+
+	if (argc != 2)
+		return (1);
+	u = argv[1];
+	len = strlen(u);
+	k[0] = l[(len ^ 59) & 63];
+	for (i = 0, r = 0; i < len; i++)
+		r += u[i];
+	k[1] = l[(r ^ 79) & 63];
+	for (i = 0, r = 1; i < len; i++)
+		r *= u[i];
+	k[2] = l[(r ^ 85) & 63];
+	for (i = 0, r = u[0]; i < len; i++)
+		if (r < u[i])
+			r = u[i];
+	srand(r ^ 14);
+	k[3] = l[rand() & 63];
+	for (i = 0, r = 0; i < len; i++)
+		r += (u[i] * u[i]);
+	k[4] = l[(r ^ 239) & 63];
+	for (i = 0, r = 0; i < u[0]; i++)
+		r = rand();
+	k[5] = l[(r ^ 229) & 63];
+	k[6] = '\0';
+	printf("%s", k);
+	return (0);
+}
