e818cf4bdd01e4c7bb73843c8140bab844a8657c
reem <ryma6110@gmail.com>
2026-04-25 14:33:58 +0000

    Add keygen for crackme5

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
 /**
- * main - Generates a key for crackme5.
- * @ac: The number of arguments.
- * @av: The arguments vector.
- * Return: 0 on success, 1 on error.
+ * main - Keygen for crackme5
+ * @ac: argument count
+ * @av: argument vector (av[1] is the username)
+ * Return: 0 on success
  */
-int main(int ac, char **av)
+int main(int ac, char *av[])
 {
-	char *u, *l = "A-CHRDwG信Df.RLhSAX9876543210";
-	char k[7];
-	int i, len = 0, res = 0;
+	unsigned int i, sum;
+	char *username;
+	char key[7];
+	char *lookup = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU4m163oejukq9fIhPaWdZ87";
 
 	if (ac != 2)
 		return (1);
-	u = av[1];
-	while (u[len])
-		len++;
-	k[0] = l[(len ^ 59) & 63];
-	for (i = 0, res = 0; i < len; i++)
-		res += u[i];
-	k[1] = l[(res ^ 79) & 63];
-	for (i = 0, res = 1; i < len; i++)
-		res *= u[i];
-	k[2] = l[(res ^ 85) & 63];
-	for (res = u[0], i = 0; i < len; i++)
-		if (u[i] > res)
-			res = u[i];
-	srand(res ^ 14);
-	k[3] = l[rand() & 63];
-	for (res = 0, i = 0; i < len; i++)
-		res += (u[i] * u[i]);
-	k[4] = l[(res ^ 239) & 63];
-	for (res = 0, i = 0; i < u[0]; i++)
-		res = rand();
-	k[5] = l[(res ^ 229) & 63];
-	k[6] = '\0';
-	printf("%s", k);
+
+	username = av[1];
+	sum = 0;
+
+	for (i = 0; i < strlen(username); i++)
+		sum += username[i];
+	key[0] = lookup[(sum ^ 59) & 63];
+	key[1] = lookup[(sum ^ 79) & 63];
+	key[2] = lookup[(sum ^ 85) & 63];
+	key[3] = lookup[(sum ^ 90) & 63];
+	key[4] = lookup[(sum ^ 91) & 63];
+	key[5] = lookup[(sum ^ 92) & 63];
+	key[6] = '\0';
+
+	printf("%s", key);
+
 	return (0);
 }
