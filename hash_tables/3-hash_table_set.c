558c91c8856f270918856b2c51ec144b5d42dd47
ryma976 <ryma6110@gmail.com>
2026-04-28 12:36:37 +0000

    Split hash_table_set into two functions to pass Betty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,19 +1,48 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds an element to the hash table.
- * @ht: The hash table to add/update the key/value to.
- * @key: The key (cannot be an empty string).
- * @value: The value associated with the key.
+ * create_node - Creates a new hash node.
+ * @key: The key for the new node.
+ * @value: The value for the new node.
  *
- * Return: 1 if it succeeded, 0 otherwise.
+ * Return: Pointer to the new node, or NULL on failure.
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (NULL);
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (NULL);
+	}
+	return (new);
+}
+
+/**
+ * hash_table_set - Adds or updates an element in the hash table.
+ * @ht: The hash table.
+ * @key: The key.
+ * @value: The value.
+ *
+ * Return: 1 if success, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *v_copy;
 	unsigned long int idx;
-	hash_node_t *tmp;
+	hash_node_t *tmp, *new;
+	char *v_copy;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
@@ -32,22 +61,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		tmp = tmp->next;
 	}
-	new = malloc(sizeof(hash_node_t));
+	new = create_node(key, value);
 	if (new == NULL)
 		return (0);
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = strdup(value);
-	if (new->value == NULL)
-	{
-		free(new->key);
-		free(new);
-		return (0);
-	}
 	new->next = ht->array[idx];
 	ht->array[idx] = new;
 	return (1);
